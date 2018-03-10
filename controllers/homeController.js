d3cfeb71f7f73d279a3946493eb9ff9458b79a52
1312070 <1312070@student.hcmus.edu.vn>
2018-03-10 17:51:23 +0700

    Update new version using API

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var find = require('arraysearch').Finder;
 
 var data, numPage, firstIndex, endIndex;
 
@@ -21,129 +20,42 @@ function compareZA(a,b) {
 
 module.exports = function(app){
 	app.get('/', function(req, res){
-		request('https://api.punkapi.com/v2/beers', function(err, response, body){
+		var url = 'https://api.punkapi.com/v2/beers?per_page=9&';
+		if (req.query) {
+			var query = req.query;
+			Object.keys(query).forEach(key => {
+				if (query[key])
+					url = url + key + "=" + query[key] + "&";
+			});
+		}
+
+		request(url, function(err, response, body){
 			data = JSON.parse(body);
-			firstIndex = 0;
-			if (parseInt(data.length % 6) == 0){
-				numPage = parseInt(data.length / 6);
-			} else {
-				numPage = parseInt(data.length / 6) + 1;
-			}
-			if (firstIndex + 6 <= data.length){
-				endIndex = firstIndex + 6;
-			} else {
-				endIndex = data.length;
-			}
-			let result = [];
-			for (let i = 0; i < endIndex; i++) {
-				result.push(data[i]);
-			}
-			res.render('home', {data: result, numPage: numPage, pageId: 1, keyword: null});
+			res.render('home', {data: data});
 		});
 	});
 
 	app.get('/getpost/:id', function(req, res){
-		request('https://api.punkapi.com/v2/beers', function(err, response, body){
+		var url = 'https://api.punkapi.com/v2/beers/' + req.params.id;
+		request(url, function(err, response, body){
 			data = JSON.parse(body);
-			var post1 = data.find(post => post.id == req.params.id );
-			res.render('post', {post: post1});
+			res.render('post', {post: data});
 		});
 	});
 
-	app.get('/getposts/:keyword', function(req, res){
-		request('https://api.punkapi.com/v2/beers', function(err, response, body){
-			data = JSON.parse(body);
-			var posts = data.filter(
-				post => post.id == req.params.keyword || 
-				post.name.toUpperCase().search(req.params.keyword.toUpperCase()) != -1);
-			firstIndex = 0;
-			if (parseInt(posts.length % 6) == 0){
-				numPage = parseInt(posts.length / 6);
-			} else {
-				numPage = parseInt(posts.length / 6) + 1;
-			}
-
-			if (firstIndex + 6 <= posts.length){
-				endIndex = firstIndex + 6;
-			} else {
-				endIndex = posts.length;
-			}
-
-			let result = [];
-			for (let i = 0; i < endIndex; i++) {
-				result.push(posts[i]);
-			}
-			res.render('home', {data: result, numPage: numPage, pageId: 1, keyword: req.params.keyword});
-		});
-	});
-
-	app.get('/page/:pageId/:keyword?', function(req, res){
-		if (req.params.keyword == null) {
-			request('https://api.punkapi.com/v2/beers', function(err, response, body){
-				data = JSON.parse(body);
-				firstIndex = (req.params.pageId - 1) * 6;
-				
-				if (parseInt(data.length % 6) == 0){
-					numPage = parseInt(data.length / 6);
-				} else {
-					numPage = parseInt(data.length / 6) + 1;
-				}
-				
-				if (firstIndex + 6 <= data.length){
-					endIndex = firstIndex + 6;
-				} else {
-					endIndex = data.length;
-				}
-				
-				let result = [];
-				for (let i = firstIndex; i < endIndex; i++) {
-					result.push(data[i]);
-				}
-
-				res.render('home', {data: result, numPage: numPage, pageId: req.params.pageId, keyword: req.params.keyword});
+	app.get('/search', function(req,res){
+		var url = 'https://api.punkapi.com/v2/beers?per_page=9&';
+		if (req.query) {
+			var query = req.query;
+			Object.keys(query).forEach(key => {
+				if (query[key])
+					url = url + key + "=" + query[key] + "&";
 			});
 		}
-		else {
-			request('https://api.punkapi.com/v2/beers', function(err, response, body){
-				data = JSON.parse(body);
-				var posts = data.filter(
-					post => post.id == req.params.keyword || 
-					post.name.toUpperCase().search(req.params.keyword.toUpperCase()) != -1);
-				firstIndex = (req.params.pageId - 1) * 6;
-				if (parseInt(data.length % 6) == 0){
-					numPage = parseInt(data.length / 6);
-				} else {
-					numPage = parseInt(data.length / 6) + 1;
-				}
-
-				if (firstIndex + 6 <= data.length){
-					endIndex = firstIndex + 6;
-				} else {
-					endIndex = data.length;
-				}
-
-				let result = [];
-				for (let i = firstIndex; i < endIndex; i++) {
-					result.push(data[i]);
-				}
-				res.render('home', {data: result, numPage: numPage, pageId: req.params.pageId, keyword: req.params.keyword});
-			});
-		}
-	});
-
-	app.get('/sortAZ', function(req, res){
-		request('https://api.punkapi.com/v2/beers', function(err, response, body){
+		
+		request(url, function(err, response, body){
 			data = JSON.parse(body);
-			data.sort(compareAZ);
-			res.render('sort', {data: data});
+			res.render('home', {data: data});
 		});
-	});
-
-	app.get('/sortZA', function(req, res){
-		request('https://api.punkapi.com/v2/beers', function(err, response, body){
-			data = JSON.parse(body);
-			data.sort(compareZA);
-			res.render('sort', {data: data});
-		});
-	});
+	})
 };
--- public/imp.js
+++ public/imp.js
@@ -5,16 +5,41 @@ $(document).ready(function(){
 		this.setAttribute("href",urll);
 	});
 
-	$('.search-post').on('click', function(){
-		let keyword = $('#searchBar').val();
-		let urll = '/getposts/' + keyword;
-		this.setAttribute("href", urll);
+	$('#pagination-here').bootpag({
+	    total: 26,          
+	    page: 1,            
+	    maxVisible: 5,     
+	    leaps: true,
 	});
 
-	$('.page').on('click', function(){
-		let pageId = this.text;
-		let keyword = $('#searchBar').val() || '';
-		let urll = '/page/' + pageId + '/' + keyword;
-		this.setAttribute("href", urll);
+	$('#pagination-here').on("page", function(event, num){
+		//event.preventDefault()
+	    //show / hide content or pull via ajax etc
+	    var urll = 'https://api.punkapi.com/v2/beers?per_page=9&';
+	    var param = window.location.search.slice(1).split("&");
+	    for (let i = 0; i < param.length; i++){
+	    	var delimiteIndex = param[i].indexOf("=");
+	    	var key = param[i].substr(0, delimiteIndex);
+	    	var value = param[i].substr(delimiteIndex + 1);
+	    	if (value) {
+	    		urll = urll + key + '=' + value + "&";
+	    	}
+	    }
+	    urll = urll + "page=" + num;
+	    //this.setAttribute("href", urll);
+	    $.ajax({
+	    	type: 'GET',
+	    	url: urll,
+	    	dataType: 'json',
+	    	success: function(data){
+	    		console.log(data);
+	    		$("#content").html('<%- include partials/content.ejs %>');
+	    	},
+	    	error: function(err){
+	    		alert(this.url);
+	    	}
+	    });
+	    
 	});
+	
 });
